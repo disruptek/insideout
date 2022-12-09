8211984211024d0b5d4d0a520e9a31e0fa0baabb
Smooth Operator <github@andy.disruptek.com>
2022-12-09 13:24:26 -0500

    tryRecv, trySend

--- insideout/mailboxes.nim
+++ insideout/mailboxes.nim
@@ -33,23 +33,27 @@ proc `==`*[A, B](a: Mailbox[A]; b: Mailbox[B]): bool =
   ## two mailboxes are identical if they have the same hash
   hash(a) == hash(b)
 
-proc `$`*(mail: Mailbox): string =
-  `&`"<mail:":
-    if mail.isInitialized:
-      hash(mail).int.toHex(6) & ">"
-    else:
-      "nil>"
-
-proc assertInitialized*(mail: Mailbox) =
-  ## raise a ValueError if the mailbox is not initialized
-  if unlikely (not mail.isInitialized):
-    raise ValueError.newException "mailbox uninitialized"
-
 proc owners*[T](mail: Mailbox[T]): int =
   ## returns the number of owners; this value is positive for
   ## initialized mailboxen and zero for all others
   if mail.isInitialized:
     result = load(mail.box.rc, moAcquire) + 1
+
+proc `$`*(mail: Mailbox): string =
+  if mail.isInitialized:
+    result = "<box:"
+    result.add:
+      hash(mail).int.toHex(6)
+    result.add: "#"
+    result.add: $mail.owners
+    result.add ">"
+  else:
+    result.add "<box:nil>"
+
+proc assertInitialized*(mail: Mailbox) =
+  ## raise a ValueError if the mailbox is not initialized
+  if unlikely (not mail.isInitialized):
+    raise ValueError.newException "mailbox uninitialized"
 
 proc `=destroy`*[T](box: var MailboxObj[T]) =
   deinitLock box.lock
@@ -81,6 +85,12 @@ proc `=copy`*[T](dest: var Mailbox[T]; src: Mailbox[T]) =
     `=destroy`(dest)
   dest.box = src.box
 
+proc len*[T](mail: Mailbox[T]): int =
+  ## length of the mailbox
+  assertInitialized mail
+  withLock mail.box.lock:
+    result = len mail.box.deck
+
 proc forget*(mail: Mailbox) {.deprecated: "debugging tool".} =
   ## cheat mode
   if mail.isInitialized:
@@ -90,7 +100,7 @@ proc forget*(mail: Mailbox) {.deprecated: "debugging tool".} =
       let was = fetchSub(mail.box.rc, 1)
       echo "forget " & $mail & "; counter now " & $(was - 1) & " in " & $getThreadId()
 
-proc newMailbox*[T](initialSize: int = defaultInitialSize): Mailbox[T] =
+proc newMailbox*[T](initialSize: Positive = defaultInitialSize): Mailbox[T] =
   ## create a new mailbox which can hold `initialSize` items
   result.box = cast[ptr MailboxObj[T]](allocShared0(sizeof MailboxObj[T]))
   result.box.deck = initDeque[T](initialSize)
@@ -108,12 +118,32 @@ proc recv*[T](mail: Mailbox[T]): T =
     result = popFirst mail.box.deck
   signal mail.box.write
 
+proc tryRecv*[T](mail: Mailbox[T]; message: var T): bool =
+  ## try to pop an item from the mailbox; true if it worked
+  assertInitialized mail
+  result =
+    trySemaphore mail.box.read:
+      withLock mail.box.lock:
+        message = popFirst mail.box.deck
+  if result:
+    signal mail.box.write
+
 proc send*[T](mail: Mailbox[T]; message: sink T) =
   ## push an item into the mailbox
   assertInitialized mail
   wait mail.box.write
   withLock mail.box.lock:
-    mail.box.deck.addLast message
-    when T is ref:
-      wasMoved message
+    mail.box.deck.addLast:
+      move message
   signal mail.box.read
+
+proc trySend*[T](mail: Mailbox[T]; message: var T): bool =
+  ## try to push an item into the mailbox; true if it worked
+  assertInitialized mail
+  result =
+    trySemaphore mail.box.write:
+      withLock mail.box.lock:
+        mail.box.deck.addLast:
+          move message
+  if result:
+    signal mail.box.read
