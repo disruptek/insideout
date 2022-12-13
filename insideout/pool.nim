f2d5e70006fb4f42e0b63cea651034d8778d1358
Smooth Operator <github@andy.disruptek.com>
2022-12-13 10:10:29 -0500

    add quit, rm head

--- insideout/pool.nim
+++ insideout/pool.nim
@@ -19,7 +19,7 @@ proc prepend*[A, B](pool: var Pool[A, B]; node: PoolNode[A, B]) =
   ## work around sigmatch
   pool.list.prepend(node)
 
-proc head*[A, B](pool: Pool[A, B]): PoolNode[A, B] =
+proc head[A, B](pool: Pool[A, B]): PoolNode[A, B] =
   ## work around sigmatch
   pool.list.head
 
@@ -28,12 +28,15 @@ iterator mitems*[A, B](pool: var Pool[A, B]): var Runtime[A, B] =
   for item in pool.list.mitems:
     yield item
 
-proc drain*(pool: var Pool) =
-  ## shut down all runtimes in the pool
-  # initiate quits for all the runtimes
+proc quit*(pool: var Pool) =
+  ## initiate quits for all the runtimes
   for item in pool.mitems:
     if item.ran:
       quit item
+
+proc drain*(pool: var Pool) =
+  ## shut down all runtimes in the pool
+  quit pool
 
   # remove runtimes as they terminate
   while not pool.head.isNil:
@@ -54,8 +57,14 @@ proc fill*[A, B](pool: var Pool[A, B]): var Runtime[A, B] =
   pool.list.prepend node
   result = node.value
 
-proc spawn*[A, B](pool: var Pool[A, B]; factory: Factory[A, B]; mailbox: Mailbox[B]) =
-  pool.fill.spawn(factory, mailbox)
+when false:
+  {.warning: "nim compiler bug".}
+  proc spawn*[A, B](pool: var Pool[A, B]; factory: Factory[A, B]; mailbox: Mailbox[B]): var Runtime[A, B] =
+    result = fill pool
+    result.spawn(factory, mailbox)
+else:
+  proc spawn*[A, B](pool: var Pool[A, B]; factory: Factory[A, B]; mailbox: Mailbox[B]) =
+    (fill pool).spawn(factory, mailbox)
 
 proc newPool*[A, B](factory: Factory[A, B]; mailbox: Mailbox[B]; initialSize: Positive = 1): Pool[A, B] =
   var n = int initialSize  # allow it to reach zero
