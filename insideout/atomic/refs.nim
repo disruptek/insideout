0eebe47580e78ba067948f3c05c606f127e05cb6
Smooth Operator <github@andy.disruptek.com>
2023-01-01 13:02:24 -0500

    relax later

--- insideout/atomic/refs.nim
+++ insideout/atomic/refs.nim
@@ -16,7 +16,7 @@ proc isNil*[T](arc: AtomicRef[T]): bool {.inline.} =
 proc `=destroy`*[T](arc: var AtomicRef[T]) {.inline.} =
   mixin `=destroy`
   if not arc.isNil:
-    if 0 == fetchSub(arc.reference.rc, 1, moAcquire):
+    if 0 == fetchSub(arc.reference.rc, 1):
       `=destroy`(arc.reference[])
       deallocShared arc.reference
       arc.reference = nil
@@ -35,7 +35,7 @@ proc owners*[T](arc: AtomicRef[T]): int {.inline.} =
   ## returns the number of owners; this value is positive for
   ## initialized references and zero for all others
   if not arc.isNil:
-    result = load(arc.reference.rc, moAcquire) + 1
+    result = load(arc.reference.rc) + 1
     if result <= 0:
       raise Defect.newException "atomic ref underrun: " & $result
 
@@ -44,7 +44,7 @@ proc new*[T](arc: var AtomicRef[T]) {.inline.} =
     arc.reference = cast[ptr Reference[T]](allocShared0(sizeof Reference[T]))
     # NOTE: we cannot destroy/reset the arc.reference.value here because
     #       it might contain a thread or mutex or condvar, etc.
-    #store(arc.reference.rc, 0, moRelease)
+    #store(arc.reference.rc, 0)
   else:
     raise ValueError.newException "attempt to reinitialize atomic ref"
 
