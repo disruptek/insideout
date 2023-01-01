6beeb5e018cd6779d9811318eea84df703885d07
Smooth Operator <github@andy.disruptek.com>
2023-01-01 12:00:44 -0500

    speed up semaphore test

--- tests/tsemaphores.nim
+++ tests/tsemaphores.nim
@@ -1,27 +1,24 @@
-import pkg/balls
-
 import insideout/semaphores
 
 proc main =
-  suite "semaphores":
-    var s: Semaphore
-    initSemaphore(s, 2)
-    check s.available == 2
-    check s.isReady
-    inc s
-    check s.isReady
-    check s.available == 3
-    dec s; dec s; dec s;
-    check s.available == 0
-    check not s.isReady
-    signal s
-    check s.isReady
-    check s.available == 1
-    wait s
-    check s.available == 0
-    inc s
-    withSemaphore s:
-      discard
+  var s: Semaphore
+  initSemaphore(s, 2)
+  doAssert s.available == 2
+  doAssert s.isReady
+  inc s
+  doAssert s.isReady
+  doAssert s.available == 3
+  dec s; dec s; dec s;
+  doAssert s.available == 0
+  doAssert not s.isReady
+  signal s
+  doAssert s.isReady
+  doAssert s.available == 1
+  wait s
+  doAssert s.available == 0
+  inc s
+  withSemaphore s:
+    discard
 
 when isMainModule:
   main()
