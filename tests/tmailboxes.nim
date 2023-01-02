51ea2fa537ec9fe28fb08e1e64c225b6d3d33070
Smooth Operator <github@andy.disruptek.com>
2023-01-02 14:05:11 -0500

    rm unittest machinery from mailbox test

--- tests/tmailboxes.nim
+++ tests/tmailboxes.nim
@@ -1,5 +1,3 @@
-import pkg/balls
-
 import insideout/mailboxes
 
 type
@@ -19,84 +17,87 @@ proc main =
   block balls_breaks_destructor_semantics:
     block:
       ## basics
-      check box.isNil
-      expect ValueError:
+      doAssert box.isNil
+      try:
         box.assertInitialized
-      check box.owners == 0
-      check box == bix
+        raise Defect.newException "expected ValueError"
+      except ValueError:
+        discard
+      doAssert box.owners == 0
+      doAssert box == bix
       box = newMailbox[RS](2)
       box.assertInitialized
-      check box != bix
-      check box.owners == 1
+      doAssert box != bix
+      doAssert box.owners == 1
       bix = box
-      check box.owners == 2
+      doAssert box.owners == 2
       bix.assertInitialized
-      check bix.owners == 2
+      doAssert bix.owners == 2
     block:
       ## send A
       var message = rs"hello"
       box.send message
-      check box.len == 1
+      doAssert box.len == 1
     block:
       ## recv B
       let message = bix.recv
-      check message == rs"hello"
-      check box.len == 0
+      doAssert message == rs"hello"
+      doAssert box.len == 0
     block:
       ## send B
       var message = rs"goodbye"
       bix.send message
-      check box.len == 1
+      doAssert box.len == 1
     block:
       ## recv A
       let message = box.recv
-      check message != "peace"
-      check message == "goodbye"
-      check box.len == 0
+      doAssert message != "peace"
+      doAssert message == "goodbye"
+      doAssert box.len == 0
     block:
       ## copy
       var bax = bix
-      check bax.owners == 3, "after copy"
+      doAssert bax.owners == 3, "after copy"
       var message = rs"sup dawg"
       bax.send message
-      check bax.owners == 3, "after send"
-      check box.len == 1
+      doAssert bax.owners == 3, "after send"
+      doAssert box.len == 1
     block:
       ## ownership
-      check bix.owners == 2
+      doAssert bix.owners == 2
       let message = recv box
-      check message == "sup dawg"
-      check box.len == 0
+      doAssert message == "sup dawg"
+      doAssert box.len == 0
     block:
       ## missing mailboxes
-      check missing == MissingMailbox
-      check missing.isNil
-      check MissingMailbox.isNil
+      doAssert missing == MissingMailbox
+      doAssert missing.isNil
+      doAssert MissingMailbox.isNil
     block:
       ## try modes
       var one = rs"one"
       var two = rs"two"
       var tres = rs"tres"
-      check box.len == 0
-      check box.trySend one
-      check box.len == 1
-      check box.trySend two
-      check box.len == 2
-      check not box.trySend tres
-      check box.len == 2
+      doAssert box.len == 0
+      doAssert box.trySend one
+      doAssert box.len == 1
+      doAssert box.trySend two
+      doAssert box.len == 2
+      doAssert not box.trySend tres
+      doAssert box.len == 2
       var message: RS
-      check box.len == 2
-      check box.tryRecv(message)
-      check box.len == 1
-      check box.tryRecv(message)
-      check box.len == 0
-      check not box.tryRecv(message)
-      check box.len == 0
+      doAssert box.len == 2
+      doAssert box.tryRecv(message)
+      doAssert box.len == 1
+      doAssert box.tryRecv(message)
+      doAssert box.len == 0
+      doAssert not box.tryRecv(message)
+      doAssert box.len == 0
     block:
       ## destructors
-      check box.owners == 2, "expected 2 owners; it's " & $box.owners
+      doAssert box.owners == 2, "expected 2 owners; it's " & $box.owners
       bix = missing
-      check box.owners == 1, "expected 1 owner; it's " & $box.owners
+      doAssert box.owners == 1, "expected 1 owner; it's " & $box.owners
 
 when isMainModule:
   main()
