be6bea784be2590f8c12ad9127035697e43dff6f
Smooth Operator <github@andy.disruptek.com>
2024-01-19 17:51:17 -0500

    rewrite for nimskull

--- insideout/atomic/flags.nim
+++ insideout/atomic/flags.nim
@@ -4,70 +4,87 @@ import std/atomics
 
 import insideout/futex
 
-type
-  AtomicFlags*[T] = Atomic[uint32]
+# this is a bit loony but we want to support nimskull, so...
 
-macro getFlags*(flags: var AtomicFlags): uint32 =
+type
+  AtomicFlags*[T] = Atomic[T]
+
+macro getFlags*[T](flags: var AtomicFlags[T]): T =
   newCall(bindSym"load", flags, bindSym"moSequentiallyConsistent")
 
-template flagType*[T](flag: T): untyped =
-  when sizeof(T) <= 4:
+template flagType*(flag: typedesc): untyped =
+  when sizeof(flag) <= 2:
+    uint16
+  elif sizeof(flag) <= 4:
     uint32
+  elif sizeof(flag) <= 8:
+    uint64
   else:
-    {.error: "flag type must be <= 4 bytes".}
+    {.error: "bad idea".}
 
-template toFlag*[T](flag: T): untyped =
-  uint32(1) shl ord(flag)
+template flagType*[V](flag: V): untyped =
+  flagType(typeOf flag)
 
-proc toFlags*[T](value: uint32): set[T] {.discardable.} =
+proc toFlag*[V](flag: V): auto =
+  flagType(flag)(1) shl ord(flag)
+
+proc toFlags*[T; V](value: T): set[V] {.discardable.} =
   when nimvm:
-    for flag in T.items:
+    for flag in V.items:
       if 0 != (value and flag.toFlag):
         result.incl flag
   else:
-    result = cast[set[T]](value)
+    result = cast[set[V]](value)
 
-proc toFlags*[T](flags: set[T]): uint32 =
+proc toFlags*[T; V](flags: set[V]): T =
   for flag in flags.items:
     result = result or flag.toFlag
 
-proc toFlags*[T](flags: var AtomicFlags[T]): set[T] {.discardable.} =
-  toFlags[T](getFlags(flags))
+proc toFlags*[T; V](flags: var AtomicFlags[T]): set[V] {.discardable.} =
+  toFlags[T, V](getFlags(flags))
 
-proc contains*[T](flags: var AtomicFlags[T]; one: T): bool =
+proc contains*[T; V](flags: var AtomicFlags[T]; one: V): bool =
   0 != (getFlags(flags) and one.toFlag)
 
-proc contains*[T](flags: var AtomicFlags[T]; many: set[T]): bool =
-  0 != (getFlags(flags) and toFlags[T](many))
+proc contains*[T; V](flags: var AtomicFlags[T]; many: set[V]): bool =
+  0 != (getFlags(flags) and toFlags[T, V](many))
 
-macro `|=`*[T](flags: var AtomicFlags[T]; one: T): set[T] =
-  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", getTypeInst(one))
+macro `|=`*[T; V](flags: var AtomicFlags[T]; one: V): set[V] =
+  let fType = newCall(bindSym"flagType", one)
+  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", fType,
+                                       getTypeInst(one))
   newCall(toFlags, newCall(bindSym"fetchOr", flags,
           newCall(bindSym"toFlag", one), bindSym"moSequentiallyConsistent"))
 
-macro `^=`*[T](flags: var AtomicFlags[T]; one: T): set[T] =
-  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", getTypeInst(one))
+macro `^=`*[T; V](flags: var AtomicFlags[T]; one: V): set[V] =
+  let fType = newCall(bindSym"flagType", one)
+  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", fType,
+                                       getTypeInst(one))
   newCall(toFlags, newCall(bindSym"fetchXor", flags,
           newCall(bindSym"toFlag", one), bindSym"moSequentiallyConsistent"))
 
-macro `|=`*[T](flags: var AtomicFlags[T]; many: set[T]): set[T] =
+macro `|=`*[T; V](flags: var AtomicFlags[T]; many: set[V]): set[V] =
   let t = getTypeInst(many)[^1]
-  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", t)
+  let fType = newCall(bindSym"flagType", t)
+  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", fType, t)
   newCall(toFlags, newCall(bindSym"fetchOr", flags,
-          newCall(bindSym"toFlags", many), bindSym"moSequentiallyConsistent"))
+          newCall(toFlags, many), bindSym"moSequentiallyConsistent"))
 
-macro `^=`*[T](flags: var AtomicFlags[T]; many: set[T]): set[T] =
+macro `^=`*[T; V](flags: var AtomicFlags[T]; many: set[V]): set[V] =
   let t = getTypeInst(many)[^1]
-  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", t)
+  let fType = newCall(bindSym"flagType", t)
+  let toFlags = nnkBracketExpr.newTree(bindSym"toFlags", fType, t)
   newCall(toFlags, newCall(bindSym"fetchXor", flags,
           newCall(toFlags, many), bindSym"moSequentiallyConsistent"))
 
-proc waitMask*[T](flags: var AtomicFlags[T]; mask: set[T]): cint {.discardable, inline.} =
-  waitMask(flags, toFlags[T](mask))
+proc waitMask*[T; V](flags: var AtomicFlags[T]; mask: set[V]): cint
+  {.discardable, inline.} =
+  waitMask(flags, toFlags[T, V](mask))
 
-proc waitMask*[T](flags: var AtomicFlags[T]; compare: uint32; mask: set[T]): cint {.discardable, inline.} =
-  waitMask(flags, cast[AtomicFlags[T]](compare), toFlags[T](mask))
+proc waitMask*[T; V](flags: var AtomicFlags[T]; compare: T;
+                     mask: set[V]): cint {.discardable, inline.} =
+  waitMask(flags, cast[AtomicFlags[T]](compare), toFlags[T, V](mask))
 
-proc wakeMask*[T](flags: var AtomicFlags[T]; mask: set[T];
+proc wakeMask*[T; V](flags: var AtomicFlags[T]; mask: set[V];
                   count = high(cint)): cint {.discardable, inline.} =
-  wakeMask(flags, toFlags[T](mask), count = count)
+  wakeMask(flags, toFlags[T, V](mask), count = count)
