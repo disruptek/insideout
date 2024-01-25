ec464a6c17464bee7e217625c1b4a7adb6671973
Smooth Operator <github@andy.disruptek.com>
2024-01-24 20:43:08 -0500

    clean-up

--- tests/4/tgoto.nim
+++ tests/4/tgoto.nim
@@ -4,17 +4,15 @@ import pkg/cps
 import pkg/insideout
 
 proc visit(home, away: UnboundedFifo[Continuation]) {.cps: Continuation.} =
-  echo "i am @ ", getThreadId()
+  echo "i am @ ", getThreadId(), " (home)"
   goto away
-  echo "i am @ ", getThreadId()
+  echo "i am @ ", getThreadId(), " (away)"
   echo "leaving visit"
 
 proc server(box: UnboundedFifo[Continuation]) {.cps: Continuation.} =
   while true:
-    sleep 1000
     var visitor: Continuation
     var receipt: WardFlag = tryRecv(box, visitor)
-    debugEcho receipt
     case receipt
     of Paused, Empty:
       if not waitForPoppable(box):
@@ -25,33 +23,24 @@ proc server(box: UnboundedFifo[Continuation]) {.cps: Continuation.} =
       discard trampoline(move visitor)
     else:
       discard
+    doAssert visitor.isNil
   echo "server exit"
 
 const Service = whelp server
 
 proc application(home: UnboundedFifo[Continuation]) {.cps: Continuation.} =
-  echo "i am @ ", getThreadId()
+  echo "i am @ ", getThreadId(), " (home)"
   var away = newUnboundedFifo[Continuation]()
   var service = Service.spawn(away)
+  echo "i am @ ", getThreadId(), " (home)"
   visit(home, away)
-  sleep 100
-  echo "i am @ ", getThreadId()
-  sleep 100
+  echo "i am @ ", getThreadId(), " (away)"
   echo "going home"
   goto home
-  echo "i'm home"
-  sleep 100
-  echo "i am @ ", getThreadId()
+  echo "i am @ ", getThreadId(), " (home)"
   disablePush away
   echo "disabled push"
-  sleep 100
-
-  stop service
-  echo "stopped service"
-  sleep 100
-  cancel service
-  echo "cancelled service"
-  sleep 100
+  join service
   echo "exit application"
 
 proc main =
@@ -61,10 +50,10 @@ proc main =
     discard trampoline(move c)
     doAssert c.isNil
     echo "listening for app"
-    sleep 2000
     c = recv home
     echo "got something"
     discard trampoline(move c)
+    doAssert c.isNil
     echo "application complete"
   echo "program exit"
 
