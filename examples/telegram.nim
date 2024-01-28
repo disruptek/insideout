6d3583e379c9a569ad5dae85b807c330edc90e9a
Smooth Operator <github@andy.disruptek.com>
2024-01-28 18:24:39 -0500

    make telegram example more interesting

--- examples/telegram.nim
+++ examples/telegram.nim
@@ -1,106 +1,166 @@
-# telegram example of flow-based programming
+## Telegram example of Flow-Based Programming
+##
+## https://en.wikipedia.org/wiki/Flow-based_programming
+
+##     In computer programming, flow-based programming (FBP) is a
+##     programming paradigm that defines applications as networks of black
+##     box processes, which exchange data across predefined connections
+##     by message passing, where the connections are specified externally
+##     to the processes. These black box processes can be reconnected
+##     endlessly to form different applications without having to be
+##     changed internally. FBP is thus naturally component-oriented.
+
+##     FBP is a particular form of dataflow programming based on bounded
+##     buffers, information packets with defined lifetimes, named ports,
+##     and separate definition of connections.
+
+## https://en.wikipedia.org/wiki/Flow-based_programming#%22Telegram_Problem%22
+##
+
+##     FBP components often form complementary pairs. This example
+##     uses two such pairs. The problem described seems very simple as
+##     described in words, but in fact is surprisingly difficult to
+##     accomplish using conventional procedural logic. The task, called
+##     the "Telegram Problem", originally described by Peter Naur, is to
+##     write a program which accepts lines of text and generates output
+##     lines containing as many words as possible, where the number of
+##     characters in each line does not exceed a certain length. The words
+##     may not be split and we assume no word is longer than the size of
+##     the output lines. This is analogous to the word-wrapping problem in
+##     text editors.
+
+## this example exhibits the following features:
+## - backpressure controls flow of data
+## - lock-free mailboxes move data
+## - components run in their own threads
+## - component communication is asynchronous
+
 import std/strutils
 import pkg/cps
 
 import insideout
 
 type
-  IP = ref string
-  Port = UnboundedFifo[IP]
+  IP = ref string          ## Information Packet in Flow-Based Programming parlance
+  Port = Mailbox[IP]       ## Port in Flow-Based Programming parlance
 
 proc ip(s: string): IP =
+  ## create a new Information Packet
   result = new string
   result[] = s
 
+proc logAs(name: string): proc(text: varargs[string, `$`]) =
+  ## create a debugging function to output thread/name
+  result =
+    proc(text: varargs[string, `$`]) =
+      var output: string
+      for s in text.items: output &= s
+      debugEcho "$# $#: $#" % [$getThreadId(), name, output]
+
 proc reader(filename: string; output: Port) {.cps: Continuation.} =
-  debugEcho getThreadId(), " reader starting"
+  ## read `filename` line by line and send each line as an IP to `output` port
+  let debug = logAs"reader"
+  debug "(start)"
   let stream = open(filename, fmRead)
   for line in stream.lines:
-    debugEcho "read> ", line
-    try:
-      output.send(ip line)
-    except ValueError:
-      break
+    debug "-> ", line
+    output.send(ip line)     # blocks if the port is full
   close stream
-  disablePush output
-  debugEcho getThreadId(), " reader stopping"
+  disablePush output         # close the port to signify end of data
+  debug "(stop)"
 
 proc decomposer(input: Port; output: Port) {.cps: Continuation.} =
-  debugEcho getThreadId(), " decomposer starting"
+  ## split each IP from `input` into words send each as an IP to `output` port
+  let debug = logAs"decomposer"
+  debug "(start)"
   while true:
     try:
-      var ip = input.recv()
+      var ip = input.recv()     # blocks if the port is empty
+      debug "<- ", ip[]
       for word in ip[].split:
-        debugEcho "dc-> ", word
-        output.send(ip word)
-    except ValueError:
+        debug "-> ", word
+        output.send(ip word)    # blocks if the port is full
+    except ValueError:          # input port is closed
       break
-  disablePush output
-  debugEcho getThreadId(), " decomposer stopping"
+  disablePush output            # close the port to signify end of data
+  debug "(stop)"
 
 proc recomposer(input: Port; output: Port; size: Positive) {.cps: Continuation.} =
-  debugEcho getThreadId(), " recomposer starting"
+  let debug = logAs"recomposer"
+  debug "(start)"
   var line: string
   while true:
     try:
-      var ip = input.recv()
-      debugEcho "rc-> ", ip[]
+      var ip = input.recv()     # blocks if the port is empty
+      debug "<- ", ip[]
       if line.len + ip[].len + 1 > size:
-        output.send(ip line)
+        debug "-> ", line
+        output.send(ip line)    # blocks if the port is full
         line = ""
       else:
         if line.len > 0:
           line.add " "
         line.add ip[]
-      debugEcho "rc=> ", line
-    except ValueError:
-      output.send(ip line)
+    except ValueError:          # input port is closed
       break
-  disablePush output
-  debugEcho getThreadId(), " recomposer stopping"
+  if line != "":
+    debug "-> ", line
+    output.send(ip line)        # blocks if the port is full
+  disablePush output            # close the port to signify end of data
+  debug "(stop)"
 
-proc writer(filename: string; input: Port) {.cps: Continuation.} =
-  debugEcho getThreadId(), " writer starting"
+proc writer(input: Port; filename: string) {.cps: Continuation.} =
+  let debug = logAs"writer"
+  debug "(start)"
   let stream = open(filename, fmWrite)
   while true:
     try:
-      var ip = input.recv()
-      debugEcho "write> ", ip[]
+      var ip = input.recv()     # blocks if the port is empty
+      debug "<- ", ip[]
       stream.writeLine ip[]
-    except ValueError:
+    except ValueError:          # input port is closed
       break
   close stream
-  debugEcho getThreadId(), " writer stopping"
+  disablePop input              # close the port to signify end of data
+  debug "(stop)"
 
-proc network(pool: var Pool; components: varargs[Continuation]) =
+proc network(components: varargs[Continuation]): auto =
+  ## spawn each component and return a running thread pool
+  result = newPool ContinuationRunner
   for component in components.items:
-    var transport = newUnboundedFifo[Continuation]()
+    # create a mailbox to deliver the component to the thread for dispatch
+    var transport = newMailbox[Continuation]()
+    # put the component into the mailbox so the thread can receive it
     transport.send(component)
-    pool.spawn(ContinuationRunner, transport)
+    # spawn a thread to run the component, using the transport as input
+    result.spawn(ContinuationRunner, transport)
 
-proc main(input: string; output: string; size: Positive = 80) =
-  # create ports
-  let rseq_dc = newMailbox[IP]()
-  let dc_rc = newMailbox[IP]()
-  let rc_wseq = newMailbox[IP]()
-  # setup network
-  var pool = newPool ContinuationRunner
-  pool.network(whelp reader(input, rseq_dc),
-               whelp writer(output, rc_wseq),
-               whelp decomposer(rseq_dc, dc_rc),
-               whelp recomposer(dc_rc, rc_wseq, size))
-  debugEcho "network running"
-  # drain network
-  for component in pool.mitems:
-    join component
-  debugEcho "network complete"
+proc wrap(inputFile: string; outputFile: string; size: Positive = 80) =
+  let debug = logAs"wrap"
+  # create ports with backpressure
+  let rseq_dc = newMailbox[IP](1)             # one input line at a time
+  let dc_rc = newMailbox[IP](30)              # many words at a time
+  let rc_wseq = newMailbox[IP](1)             # one output line at a time
+  debug "setting up network"
+  block:
+    # setup and run the network in a thread pool
+    var pool = network(whelp reader(filename = inputFile, output = rseq_dc),
+                       whelp decomposer(input = rseq_dc, output = dc_rc),
+                       whelp recomposer(input = dc_rc, output = rc_wseq,
+                                        size = size),
+                       whelp writer(input = rc_wseq, filename = outputFile))
+    debug "network is running"
+  debug "network is complete"
 
-let input = "data/input.txt"
-let output = "data/output.txt"
-main(input, output, size = 40)
-debugEcho "\n"
-for line in input.lines:
-  debugEcho line
-debugEcho "\n"
-for line in output.lines:
-  debugEcho line
+when isMainModule:
+  let input = "data/input.txt"
+  let output = "data/output.txt"
+  proc dumpFile(name: string) =
+    ## dump the contents of `name` to the debug output
+    debugEcho "\n\t$#:\n" % [name]
+    for line in name.lines:
+      debugEcho line
+  # perform word-wrapping on input file; deliver to output file
+  wrap(input, output, size = 40)
+  dumpFile input
+  dumpFile output
