b931ca8e5e0275685b081310147cc570d456545a
Smooth Operator <github@andy.disruptek.com>
2024-02-01 22:53:13 -0500

    timeout attempts

--- insideout/futex.nim
+++ insideout/futex.nim
@@ -6,6 +6,7 @@
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 import std/atomics
+import std/math
 import std/posix
 
 type
@@ -36,22 +37,36 @@ const
 let NR_Futex {.importc: "__NR_futex", header: "<sys/syscall.h>".}: cint
 proc syscall(sysno: clong): cint {.header:"<unistd.h>", varargs.}
 
-proc sysFutex(futex: pointer; op: cint; val1: cint; timeout: pointer = nil;
-              val2: pointer = nil; val3: cint = 0): cint =
-  syscall(NR_Futex, futex, op, val1, timeout, val2, val3)
+proc sysFutex(futex: pointer; op: cint; val: uint32; timeout: pointer = nil;
+              uaddr2: pointer = nil; val3: uint32 = 0): cint =
+  syscall(NR_Futex, futex, op, val, timeout, uaddr2, val3)
 
 proc wait*[T](monitor: var Atomic[T]; compare: T): cint =
   ## Suspend a thread if the value of the futex is the same as refVal.
-  sysFutex(addr monitor, WaitPrivate, cast[cint](compare))
+  sysFutex(addr monitor, WaitPrivate, cast[uint32](compare))
 
 proc wait*[T](monitor: var Atomic[T]): cint =
   ## Suspend a thread until the value of the futex changes.
-  sysFutex(addr monitor, WaitPrivate, cast[cint](monitor))
+  sysFutex(addr monitor, WaitPrivate, cast[uint32](monitor))
 
 proc waitMask*[T](monitor: var Atomic[T]; compare: T; mask: uint32): cint =
   ## Suspend a thread until any of `mask` bits are set.
-  sysFutex(addr monitor, WaitBitsPrivate, cast[cint](compare),
-           val3 = cast[cint](mask))
+  sysFutex(addr monitor, WaitBitsPrivate, cast[uint32](compare), val3 = mask)
+
+proc waitMask*[T](monitor: var Atomic[T]; compare: T; mask: uint32;
+                  timeout: float): cint =
+  ## Suspend a thread until any of `mask` bits are set;
+  ## give up waiting after `timeout` seconds.
+  var tm: Timespec
+  tm.tv_sec = Time timeout.floor
+  tm.tv_nsec = clong((timeout - tm.tv_sec.float) * 1_000_000_000)
+  sysFutex(addr monitor, WaitBitsPrivate, cast[uint32](compare),
+           timeout = cast[pointer](addr tm), val3 = mask)
+
+proc waitMask*[T](monitor: var Atomic[T]; mask: uint32; timeout: float): cint =
+  ## Suspend a thread until any of `mask` bits are set;
+  ## give up waiting after `timeout` seconds.
+  waitMask(monitor, cast[T](monitor), mask, timeout)
 
 proc waitMask*[T](monitor: var Atomic[T]; mask: uint32): cint =
   ## Suspend a thread until any of `mask` bits are set.
@@ -64,21 +79,19 @@ proc wake*[T](monitor: var Atomic[T]; count = high(cint)): cint {.discardable.} 
   # or a Posix error code (if negative).
   sysFutex(addr monitor, WakePrivate, count)
 
-proc wakeMask*[T](monitor: var Atomic[T]; mask: uint32; count = high(cint)): cint {.discardable.} =
+proc wakeMask*[T](monitor: var Atomic[T]; mask: uint32; count = high(uint32)): cint {.discardable.} =
   ## Wake as many as `count` threads from the same process,
   ## which are all waiting on any set bit in `mask`.
   # Returns the number of actually woken threads
   # or a Posix error code (if negative).
-  sysFutex(addr monitor, WakeBitsPrivate, count, val3 = cast[cint](mask))
+  sysFutex(addr monitor, WakeBitsPrivate, count, val3 = cast[uint32](mask))
 
-template checkWait*(waited: cint): untyped =
-  var e = waited
-  if e < 0:
-    e = -e
-    case e
-    of EPERM:
-      stderr.write("EPERM on futex wait\n")
-    of EINTR, EAGAIN:
-      discard
-    else:
-      raise ValueError.newException $strerror(e)
+proc checkWait*(err: cint): cint {.discardable.} =
+  if err >= 0: return err
+  case errno
+  #of ETIMEDOUT:
+  #  echo getThreadId(), " stall!"
+  of EINTR, EAGAIN, ETIMEDOUT:
+    return errno
+  else:
+    raise ValueError.newException $strerror(errno)
