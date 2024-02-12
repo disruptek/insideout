5805c3f6c78c59c51d1f84eb79aa7afdcc4d46cf
Smooth Operator <github@andy.disruptek.com>
2024-02-11 19:17:23 -0500

    remove backlog from the runtimes test for now

--- tests/3/truntimes.nim
+++ tests/3/truntimes.nim
@@ -6,14 +6,14 @@ import pkg/cps
 
 import insideout/runtimes
 import insideout/mailboxes
-import insideout/backlog
+#import insideout/backlog
 import insideout/valgrind
 
 let N =
   if getEnv"GITHUB_ACTIONS" == "true" or not defined(danger) or isGrinding():
     10_000
   else:
-    100_000
+    50_000
 
 type
   Server = ref object of Continuation
@@ -23,31 +23,31 @@ proc cooperate(c: Continuation): Continuation {.cpsMagic.} = c
 
 proc unblocking(jobs: Mailbox[Job]) {.cps: Server.} =
   ## non-blocking receive
-  debug "service began"
+  #debug "service began"
   while true:
     var job: Job
     case jobs.tryRecv(job)
     of Received:
       discard
     of Unreadable:
-      debug "service lost input"
+      #debug "service lost input"
       break
     else:
-      debug "service waiting"
+      #debug "service waiting"
       if not jobs.waitForPoppable():
-        debug "service wait failed"
+        #debug "service wait failed"
         break
     cooperate()
-  debug "service ended"
+  #debug "service ended"
 
 proc blocking(jobs: Mailbox[Job]) {.cps: Server.} =
   ## blocking receive
-  debug "service began"
+  #debug "service began"
   while true:
-    debug "service waiting"
+    #debug "service waiting"
     var job = recv jobs
     cooperate()
-  debug "service ended"
+  #debug "service ended"
 
 const Unblocking = whelp unblocking
 const Blocking = whelp blocking
@@ -56,27 +56,27 @@ proc main() =
 
   block:
     ## non-blocking mailbox sniffer
-    notice "runtime"
+    #notice "runtime"
     let jobs = newMailbox[Job]()
-    info "[runtime] spawn"
+    #info "[runtime] spawn"
     var runtime = Unblocking.spawn(jobs)
     var other = runtime
     doAssert other.state == Running
     doAssert other == runtime
-    info "[runtime] pin"
+    #info "[runtime] pin"
     pinToCpu(other, 0)
     doAssert runtime.mailbox == jobs
     var job = Job()
-    info "[runtime] send"
+    #info "[runtime] send"
     while Delivered != jobs.trySend(job):
       discard
-    info "[runtime] close"
+    #info "[runtime] close"
     jobs.disablePush()
-    info "[runtime] join"
+    #info "[runtime] join"
     join runtime
-    info "[runtime] done"
+    #info "[runtime] done"
 
-  block:
+  when false: #block:
     ## blocking waitor cancellation
     notice "cancellation"
     var jobs = newMailbox[Job]()
