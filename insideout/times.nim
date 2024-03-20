d4b443a73ffd21039382cd591bc510fcd54ee250
Smooth Operator <github@andy.disruptek.com>
2024-03-20 11:28:58 -0400

    add complementary operators

--- insideout/times.nim
+++ insideout/times.nim
@@ -26,9 +26,19 @@ proc toTimeSpec*(timeout: float): TimeSpec =
   result.tv_sec = Time timeout.floor
   result.tv_nsec = clong((timeout - result.tv_sec.float) * 1_000_000_000)
 
+proc toFloat*(ts: TimeSpec): float =
+  ts.tv_sec.float + ts.tv_nsec / 1_000_000_000
+
 proc `+`*(a, b: TimeSpec): TimeSpec =
   result.tv_sec = Time(a.tv_sec.clong + b.tv_sec.clong)
   result.tv_nsec = a.tv_nsec + b.tv_nsec
   if result.tv_nsec >= 1_000_000_000:
     result.tv_sec = Time(result.tv_sec.clong + 1)
     result.tv_nsec -= 1_000_000_000
+
+proc `-`*(a, b: TimeSpec): TimeSpec =
+  result.tv_sec = Time(a.tv_sec.clong - b.tv_sec.clong)
+  result.tv_nsec = a.tv_nsec - b.tv_nsec
+  if result.tv_nsec < 0:
+    result.tv_sec = Time(result.tv_sec.clong - 1)
+    result.tv_nsec += 1_000_000_000
