f7871bc97d7fab9cb00267d7d113a99429d19dbf
Smooth Operator <github@andy.disruptek.com>
2024-08-26 09:20:18 -0400

    rm doc example

--- insideout/saferlists.nim
+++ insideout/saferlists.nim
@@ -9,23 +9,6 @@ proc safeRemove*[T](L: var SinglyLinkedList[T], n: SinglyLinkedNode[T]): bool {.
   ## Attempting to remove an element not contained in the list is a no-op.
   ## Differs from stdlib's lists.remove() in that it has no special
   ## cyclic behavior which causes memory errors.  🙄
-  runnableExamples:
-    import std/[sequtils, enumerate, sugar]
-    var a = [0, 1, 2].toSinglyLinkedList
-    let n = a.head.next
-    assert n.value == 1
-    assert a.remove(n) == true
-    assert a.toSeq == [0, 2]
-    assert a.remove(n) == false
-    assert a.toSeq == [0, 2]
-    a.addMoved(a) # cycle: [0, 2, 0, 2, ...]
-    a.remove(a.head)
-    let s = collect:
-      for i, ai in enumerate(a):
-        if i == 4: break
-        ai
-    assert s == [2, 2, 2, 2]
-
   if n == L.head:
     L.head = n.next
     when false:
